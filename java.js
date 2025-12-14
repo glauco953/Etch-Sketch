8940e77f094e0bf7b7e41d7534a30ae7b35f12ea
glauco953 <gspiegas@yahoo.com.br>
2025-12-14 19:36:40 -0300

    COMPLETED THE EXERCISE!

--- java.js
+++ java.js
@@ -1,38 +1,59 @@
-const gridSize = 16;
-const squareWidthPercentage = 100 / gridSize;
+let gridSize = 16;
+let squareWidthPercentage = 100 / gridSize;
 
-let squares = [];
+const refreshHandler = function (e) {
+  e.preventDefault();
+  const newSize = document.getElementById("size").value;
+  if (newSize > 200 || newSize <= 0) {
+    alert("Invalid size");
+    return;
+  }
+  gridSize = newSize;
+  squareWidthPercentage = 100 / gridSize;
+  buildGrid();
+};
+
+const activateSquare = function (e) {
+  e.target.classList.add("active");
+};
+
 const root = document.body;
-const sandbox = document.createElement("div");
-sandbox.classList.add("sandbox");
 
-const controlPanel = document.createElement("div");
+const controlPanel = document.createElement("form");
 controlPanel.classList.add("panel");
+controlPanel.setAttribute("onSubmit", "#");
 
 const cPanelTitle = document.createElement("h2");
 cPanelTitle.textContent = "Select the grid width: ";
 controlPanel.appendChild(cPanelTitle);
 
 const gridLengthInput = document.createElement("input");
+gridLengthInput.setAttribute("id", "size");
 gridLengthInput.setAttribute("type", "number");
 controlPanel.appendChild(gridLengthInput);
 
 const refreshButton = document.createElement("button");
-refreshButton.textContent = "Clear";
+refreshButton.textContent = "Restart";
+refreshButton.addEventListener("click", refreshHandler);
 controlPanel.appendChild(refreshButton);
+root.appendChild(controlPanel);
 
-const activateSquare = function (e) {
-  e.target.classList.add("active");
-};
+function buildGrid() {
+  const oldSandbox = document.getElementById("grid");
+  if (oldSandbox) oldSandbox.remove();
+  const sandbox = document.createElement("div");
+  sandbox.classList.add("sandbox");
+  sandbox.setAttribute("id", "grid");
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.setAttribute("id", i);
+    square.style.width = squareWidthPercentage + "%";
+    square.addEventListener("mouseenter", activateSquare);
+    sandbox.appendChild(square);
+  }
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  square.setAttribute("id", i);
-  square.style.width = squareWidthPercentage + "%";
-  square.addEventListener("mouseenter", activateSquare);
-  sandbox.appendChild(square);
+  root.appendChild(sandbox);
 }
 
-root.appendChild(controlPanel);
-root.appendChild(sandbox);
+buildGrid();
