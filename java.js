21d8c2dcce1a2678c6db5a0a6bb6e166dc8706a1
glauco953 <gspiegas@yahoo.com.br>
2025-12-14 15:12:31 -0300

    REMOVE SQUARE SUBCONTAINER
    Doesn't solve the zero height problem...

--- java.js
+++ java.js
@@ -8,11 +8,8 @@ sandbox.classList.add("sandbox");
 const generateRowItems = function (rowIndex) {
   for (let columnIndex = 0; columnIndex < gridSize; columnIndex++) {
     items[rowIndex][columnIndex] = document.createElement("div");
-    let subcontainer = document.createElement("div");
-    subcontainer.classList.add("square-container");
     items[rowIndex][columnIndex].classList.add("square");
-    subcontainer.appendChild(items[rowIndex][columnIndex]);
-    rows[rowIndex].appendChild(subcontainer);
+    rows[rowIndex].appendChild(items[rowIndex][columnIndex]);
   }
 };
 
